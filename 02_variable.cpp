84ddd260ab9ad89febe69159c22121ea62260ac4
ainur naher mithun <ainurnahermithun469@gmail.com>
2026-07-25 16:44:26 +0600

    Add files via upload

---
+++ 02_variable.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age=20;
+    float height=4.11;
+    char grade='A';
+    double pie=3.1416;
+    string name="Mithun";
+
+    cout<<"Name:"<<name<<endl;
+    cout<<"Age:"<<age<<endl;
+    cout<<"Grade:"<<grade<<endl;
+    cout<<"Height:"<<height<<endl;
+    cout<<"Pie:"<<pie;
+
+
+
+
+
+
+    return 0;
+}
