eb8a41009f68c1ffcec0853f4ed3906a74608fea
ainur naher mithun <ainurnahermithun469@gmail.com>
2026-07-25 16:45:06 +0600

    Add files via upload

---
+++ 04_operators.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a=10,b=20;
+    cout<<"Addition:"<<a+b<<endl;
+    cout<<"Subtraction:"<<b-a<<endl;
+    cout<<"Multiplication:"<<a*b<<endl;
+    cout<<"Division:"<<a/b<<endl;
+    cout<<"Remainder:"<<a%b;
+
+
+    return 0;
+}
+
