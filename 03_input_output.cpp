446ea076c7477e493767e0778720e34b25295cdc
ainur naher mithun <ainurnahermithun469@gmail.com>
2026-07-25 16:44:47 +0600

    Add files via upload

---
+++ 03_input_output.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int age;
+    string name;
+    cout<<"The age is:";
+    cin>>age;
+
+    cout<<"The name is:";
+    cin>>name;
+
+    cout<<"Age:"<<age<<endl;
+    cout<<"Name:"<<name<<endl;
+
+
+
+
+    return 0;
+}
